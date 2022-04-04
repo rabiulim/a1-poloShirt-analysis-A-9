0e08e46cf6d2d1fe43f2134f9e4889597690e2b9
rabiulim <rabiulim2022@gmail.com>
2022-04-04 21:43:23 +0600

    blogs and home component done

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -3,7 +3,14 @@ import './Blogs.css'
 const Blogs = () => {
     return (
         <div className='blog'>
-            this is blog page
+            <h1 className='text-3xl'>What is contex API</h1>
+            <p className='text-xl'>The Context API is a React cool system that enables you to exchange  details and helps in solving prop-drilling from all levels of your application.Contex api system make our coding life esiar.Context provides a way to share values like these between components without having to separately  pass a prop through every level of the tree.So we can say Contex API is a great system that replace boring props drilling system . </p>
+            <h1 className="text-3xl">
+                What is Semantic Tag
+            </h1>
+            <p className='text-xl'>
+                A semantic tag clearly describes its meaning to both the browser and the developer.The semantic tag help to HTML stems from what should be the driving goal of any web page.Semantic HTML tags provide information about the contents of those tags that goes beyond just how they look on a page.Using semantic tags gives you many more hooks for styling your content too.By using semantic tags, you create code  more informative than those that simply HTML. So our should use semantic tags.
+            </p>
         </div>
     );
 };
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,8 +2,14 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div className='home-page'>
-            <img src="https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-13-mini.jpg" alt="" />
+        <div className='flex'>
+            <div>
+                <h1 className='text-5xl my-16 text-orange-400 font-bold'>A1 POLO Shirt Analysis</h1>
+                <p className='text-2xl'>Hi, you are wellcome my reveiws site. Here you find all analysis about A1 polo shirt.So if you interested about my analysis ,plz keep exploring the website </p>
+            </div>
+            <div className=''>
+                <img src="https://www.elevenworkwear.com.au/Images/ProductImages/Medium/E1424_BLCK_F1.jpg" alt="" />
+            </div>
         </div>
     );
 };
