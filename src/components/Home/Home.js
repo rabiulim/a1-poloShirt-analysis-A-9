e2bf829d403cdf55e10c7e3b9d9d6023d0fa51ee
rabiulim <rabiulim2022@gmail.com>
2022-04-05 12:02:17 +0600

    readme file edit done

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,14 +1,25 @@
 import React from 'react';
+// import useReviews from '../../hooks/useReviews';
 
 const Home = () => {
+    // const [reviews, setReviews] = useReviews();
     return (
-        <div className='flex'>
-            <div>
-                <h1 className='text-5xl my-16 text-orange-400 font-bold'>A1 POLO Shirt Analysis</h1>
-                <p className='text-2xl'>Hi, you are wellcome my reveiws site. Here you find all analysis about A1 polo shirt.So if you interested about my analysis ,plz keep exploring the website </p>
+        <div>
+            <div className='flex'>
+                <div>
+                    <h1 className='text-5xl my-16 text-orange-400 font-bold'>A1 POLO Shirt Analysis</h1>
+                    <p className='text-2xl'>Hi, you are wellcome my reveiws site. Here you find all analysis about A1 polo shirt.So if you interested about my analysis ,plz keep exploring the website </p>
+                </div>
+                <div className=''>
+                    <img src="https://www.elevenworkwear.com.au/Images/ProductImages/Medium/E1424_BLCK_F1.jpg" alt="" />
+                </div>
             </div>
-            <div className=''>
-                <img src="https://www.elevenworkwear.com.au/Images/ProductImages/Medium/E1424_BLCK_F1.jpg" alt="" />
+            <div>
+                <h1 className='text-3xl font-semibold my-12'>Customer Reviews (3)</h1>
+                {/* {
+                    homeReviews.map(review => (<li>{review.name}</li>))
+                } */}
+                <button className='rounded-full bg-red-600 px-6 py-3 text-white text-xl'>See All Reviews</button>
             </div>
         </div>
     );
